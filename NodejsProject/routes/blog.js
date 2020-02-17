eeee44393ea7396334f974db78531607be9ccb92
udomudompan <60595810+udomudompan@users.noreply.github.com>
2020-02-17 15:43:32 +0700

    add validation to Project

--- NodejsProject/routes/blog.js
+++ NodejsProject/routes/blog.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -10,9 +11,14 @@ router.get('/add', function(req, res, next) {
     res.render('addblog');
 });
 
-router.post('/add', function(req, res, next) {
-    console.log(req.body.name);
-    console.log(req.body.description);
-    console.log(req.body.author);
+router.post('/add',[
+    check("name","Please Input your blog name").not().isEmpty(),
+    check("description","Please Input your blog description").not().isEmpty(),
+    check("author","Please Input your blog author").not().isEmpty(),
+], function(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        console.log(errors);
+    }
 });
 module.exports = router;
